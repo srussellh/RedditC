780fc56cd73e8293b21efe29fcd1855403232790
srussellh <shanerherbert@gmail.com>
2019-05-22 10:31:11 -0600

    Initial Commit

---
+++ RedditC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditC
+//
+//  Created by Bobba Kadush on 5/22/19.
+//  Copyright © 2019 Bobba Kadush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RedditC
+//
+//  Created by Bobba Kadush on 5/22/19.
+//  Copyright © 2019 Bobba Kadush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
